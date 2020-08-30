65be6514b9d377d7cc8f05767d0bd696fe820f53
imran <imranmidday6666@gmail.com>
2020-08-30 13:36:12 +0530

    adding margin to the title.

--- src/containers/App.js
+++ src/containers/App.js
@@ -33,7 +33,7 @@ class App extends Component {
 		<h1 className='tc'>Loading...</h1> :
 		(
 				<div className='tc'>
-					<h1 className='f1'>RoboFriends</h1>
+					<h1 className='f1 ma3'>RoboFriends</h1>
 					<SearchBox searchchange={this.onSearchChange}/>
 					<Scroll>
 						<ErrorBoundry>
